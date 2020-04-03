c62797eb2426975453aecd4d31990c09b5e41222
Ernouf <48248463+Ernouf@users.noreply.github.com>
2020-04-03 22:28:15 +0200

    init commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timePassed = 0f;
+    private bool spaceHasBeenPressed = false;
+    public float allowedPressedFrq = 1.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && !spaceHasBeenPressed)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            spaceHasBeenPressed = true;
+        }
+
+        if(spaceHasBeenPressed) timePassed += Time.deltaTime;
+        
+        if(timePassed >= allowedPressedFrq)
+        {
+            spaceHasBeenPressed = false;
+            timePassed = 0;     
+        }
+    }
+}
